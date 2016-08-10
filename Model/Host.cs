6b5041641effcc762a9ebad3a41841bce344f69a
garbervetsky <diego.garbervetsky@gmail.com>
2016-08-09 20:25:13 -0700

    Fix of inverted Namespaces + TypeWithoutNameSpaces

--- Model/Host.cs
+++ Model/Host.cs
@@ -37,14 +37,14 @@ namespace Model
 			var assembly = this.Assemblies.SingleOrDefault(a => a.MatchReference(type.ContainingAssembly));
 			if (assembly == null) return null;
 
-			var namespaces = type.ContainingNamespace.Split('.');
+			var namespaces = type.ContainingNamespace.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
 			var containingNamespace = assembly.RootNamespace;
 
-			foreach (var name in namespaces)
-			{
-				containingNamespace = containingNamespace.Namespaces.SingleOrDefault(n => n.Name == name);
-				if (containingNamespace == null) return null;
-			}
+            foreach (var name in namespaces)
+            {
+                containingNamespace = containingNamespace.Namespaces.SingleOrDefault(n => n.Name == name);
+                if (containingNamespace == null) return null;
+            }
 			
 			var result = containingNamespace.Types.SingleOrDefault(t => t.MatchReference(type));
 			return result;
