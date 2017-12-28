c236575c1e14e5cfd9e5608137aede3be3104d5a
m7nu3l <mcarrasco@dc.uba.ar>
2017-12-28 10:49:47 -0300

    Added initial files

---
+++ TinyBCT/Program.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Edgardo Zoppi.  All Rights Reserved.  Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Cci;
+using Backend;
+
+namespace TinyBCT
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const string root = @"..\..\..";
+			//const string root = @"C:"; // casa
+			//const string root = @"C:\Users\Edgar\Projects"; // facu
+
+			const string input = root + @"\Test\bin\Debug\Test.dll";
+
+			using (var host = new PeReader.DefaultHost())
+			using (var assembly = new Assembly(host))
+			{
+				assembly.Load(input);
+
+				Types.Initialize(host);
+
+				var visitor = new MethodVisitor(host, assembly.PdbReader);
+				visitor.Traverse(assembly.Module);
+			}
+
+			System.Console.WriteLine("Done!");
+			System.Console.ReadKey();
+		}
+	}
+}
