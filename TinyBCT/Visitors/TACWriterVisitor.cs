f384461c5ef88bb8c990dd19666b8e2fb0d931e2
m7nu3l <mcarrasco@dc.uba.ar>
2018-01-29 14:23:38 -0300

    Adhoc read and write for references in heap.

--- TinyBCT/Visitors/TACWriterVisitor.cs
+++ TinyBCT/Visitors/TACWriterVisitor.cs
@@ -58,9 +58,49 @@ namespace TinyBCT
 
             transformBody(methodBody);
 
-            sb.AppendLine(methodBody.ToString());
+            sb.AppendLine(ToString(methodBody));
         }
 
+        // MethodBody's ToString copied to print type of instruction.
+        public string ToString(MethodBody methodBody)
+        {
+            var result = new StringBuilder();
+            var header = MemberHelper.GetMethodSignature(methodBody.MethodDefinition, NameFormattingOptions.Signature | NameFormattingOptions.ParameterName);
+
+            result.AppendLine(header);
+
+            foreach (var variable in methodBody.Variables)
+            {
+                var type = "unknown";
+
+                if (variable.Type != null)
+                {
+                    type = TypeHelper.GetTypeName(variable.Type);
+                }
+
+                result.AppendFormat("  {0} {1};", type, variable.Name);
+                result.AppendLine();
+            }
+
+            result.AppendLine();
+
+            foreach (var instruction in methodBody.Instructions)
+            {
+                result.Append("  ");
+                result.Append(instruction);
+                result.Append(String.Format("   //{0}", instruction.GetType()));
+                result.AppendLine();
+            }
+
+            foreach (var handler in methodBody.ExceptionInformation)
+            {
+                result.AppendLine();
+                result.Append(handler);
+            }
+
+            return result.ToString();
+        }
+   
         public override String ToString()
         {
             return sb.ToString();
