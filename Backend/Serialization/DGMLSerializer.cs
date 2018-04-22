5add4492f10a87fbe63c04112f17020c8716d01d
Edgardo Zoppi <edgarzpp@gmail.com>
2018-04-21 21:02:22 -0300

    Fixing topological sort of CFG nodes and CG methods.

--- Backend/Serialization/DGMLSerializer.cs
+++ Backend/Serialization/DGMLSerializer.cs
@@ -241,7 +241,7 @@ namespace Backend.Serialization
 			using (var stringWriter = new StringWriter())
 			using (var xmlWriter = new XmlTextWriter(stringWriter))
 			{
-				var reachableMethods = new Dictionary<IMethodReference, int>(new MethodReferenceDefinitionComparer());
+				var reachableMethods = new Dictionary<IMethodReference, int>(MethodReferenceDefinitionComparer.Default);
 
 				xmlWriter.Formatting = Formatting.Indented;
 				xmlWriter.WriteStartElement("DirectedGraph");
